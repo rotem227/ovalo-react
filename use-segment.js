5b63a5e570edeec9f01037baeefc41f071118ec9
Rotem <rotem227@gmail.com>
2022-01-28 02:41:30 +0200

    Tweak: Updating ovalo package usage according latest changes

--- use-segment.js
+++ use-segment.js
@@ -3,8 +3,8 @@ import { useState, useMemo } from 'react';
 import Store from 'ovalo';
 
 export default function useSegment( key, group ) {
-    const { state: initialState, dispatch, actions, register } = Store.useSegment( key, group );
-    const [ state, setState ] = useState( () => initialState );
+    const { initial, dispatch, actions, register } = Store.use( key, group );
+    const [ state, setState ] = useState( () => initial );
     const { unregister, restore } = useMemo( () => register( setState ), [] );
 
     return {

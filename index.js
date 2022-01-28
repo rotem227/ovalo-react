1b06142558f4f94f6581e753f5a923176216816b
Rotem <rotem227@gmail.com>
2022-01-28 10:28:33 +0200

    Tweak: Exporting the useDispatch hook

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { default as useSegmentHook } from './use-segment';
 import { default as useInitSegmentsHook } from './use-init-segments';
+import { default as useSegmentHook } from './use-segment';
+import { default as useDispatchHook } from './use-dispatch';
+
 import Segments from 'ovalo';
 
-export const useSegment = useSegmentHook;
 export const useInitSegments = useInitSegmentsHook;
+export const useSegment = useSegmentHook;
+export const useDispatch = useDispatchHook;
 
 export default Segments;
